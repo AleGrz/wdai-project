d83ceac3a5644910981c6327bb70ec31220fc255
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2025-01-06 19:56:52 +0100

    New route

---
+++ app/api/category/[categoryId]/childrenCategories/route.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from "next/server";
+
+import { PrismaClient } from "@prisma/client";
+
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: Promise<{ categoryId: string }> },
+) {
+  const prisma = new PrismaClient();
+  const id = (await params).categoryId;
+
+  if (await prisma.category.findUnique({ where: { id: id } })) {
+    const categories = await prisma.category.findMany({
+      where: { parentCategoryId: id },
+    });
+
+    return Response.json(categories);
+  } else {
+    return Response.json({ message: "Category not found!" }, { status: 404 });
+  }
+}
