c9f4be0a41df5c67e7575dd4dc9c332cd22d1ead
AleGrz <ajemalbatros@gmail.com>
2025-01-07 22:33:12 +0100

    komit

---
+++ prisma/migrations/20250107210226_rating/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "inStock" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "reviewsCount" INTEGER NOT NULL DEFAULT 0,
+    "rating" REAL NOT NULL DEFAULT 0,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("brand", "categoryId", "description", "id", "imageUrl", "inStock", "name", "price") SELECT "brand", "categoryId", "description", "id", "imageUrl", "inStock", "name", "price" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
