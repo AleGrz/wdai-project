aa257d85eaac4f940d81ab1795547fd8aa967c94
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2025-01-11 20:51:49 +0100

    Add avatar button

--- components/nav/avatarButton.tsx
+++ components/nav/avatarButton.tsx
@@ -1,22 +1,32 @@
-import { Box } from "@chakra-ui/react";
-
 import { MenuRoot, MenuContent, MenuTrigger, MenuItem } from "@/components/ui/menu";
 import { Avatar } from "@/components/ui/avatar";
 import LogoutButton from "@/components/nav/logoutButton";
+import { Button } from "@/components/ui/button";
+
+const colorPalette = ["red", "blue", "green", "yellow", "purple", "orange"]
+
+const pickPalette = (name: string | undefined) => {
+  if (!name) return colorPalette[0];
+  const index = name.charCodeAt(0) % colorPalette.length
+  
+  return colorPalette[index]
+}
 
 const AvatarButton: React.FC<{
   name?: string | undefined
 }> = ({ name }) => {
   return (
     <MenuRoot>
-      <MenuTrigger
-        asChild
-        _hover={{
+      <MenuTrigger asChild>
+        <Button
+          borderRadius="full"
+          variant="ghost"
+          aspectRatio={1}
+          _hover={{
             boxShadow: "0 0 1px 1px white"
-        }}>
-        <Box borderRadius="full">
-          <Avatar size={"sm"} name={name}  />
-        </Box>
+          }}>
+          <Avatar size={"sm"} name={name} colorPalette={pickPalette(name)} />
+        </Button>
       </MenuTrigger>
       <MenuContent>
         <MenuItem value="logout" asChild>
