1294ffe11710118fc80838c759b87f8af757913a
Jakub Jurczyk <jjurczyk@student.agh.edu.pl>
2025-01-11 20:06:33 +0100

    Fix

--- components/searchControl.tsx
+++ components/searchControl.tsx
@@ -1,38 +1,25 @@
-"use client";
-import { Input } from "@chakra-ui/react";
+import { Flex, Group, Input } from "@chakra-ui/react";
 import { RiSearch2Fill } from "react-icons/ri";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Form from 'next/form'
 
 import { Button } from "./ui/button";
 
-
 const SearchControl: React.FC = () => {
-  const [query, setQuery] = useState("");
-  const router = useRouter();
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const queryParam = urlParams.get("query");
-
-    if (window.location.pathname === "/search") {
-      setQuery(queryParam || "");
-    }
-  }, []);
-
   return (
-    <>
-      <Input 
-        placeholder="Search" 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)} 
-        onKeyDown={(e) => e.key === "Enter" && router.push(`/search?query=${query}`)}
-      />
-      <Button variant="outline" onClick={() => router.push(`/search?query=${query}`)}>
-        <RiSearch2Fill />
-      </Button>
-    </>
+    <Form action="/search">
+      <Flex gap={0} grow={1} asChild>
+        <Group attached>
+          <Input
+            placeholder="Search" 
+            aria-label="Search"
+            name="query"
+          />
+          <Button type="submit" variant="outline">
+            <RiSearch2Fill />
+          </Button>
+        </Group>
+      </Flex>
+    </Form>
   );
 }
 
